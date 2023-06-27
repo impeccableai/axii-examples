f4c3aae1cd72e45cd1e44e32bc6983fa5094c5b1
Pawel Kapica <pawel@impeccable.ai>
2023-06-27 13:58:05 +0200

    simple reader

--- tasks/reader/reader.py
+++ tasks/reader/reader.py
@@ -1,24 +1,12 @@
 import argparse
 
-
-def read_from_file(filepath):
-    l = list()
-    with open(filepath) as f:
-        for line in f:
-            l.append(int(line))
-    return l
-
-
 def main(args):
-    a = read_from_file(args.a)
-    b = read_from_file(args.b)
-
-    for i in range(min(len(a), len(b))):
-        print(a[i], b[i])
+    with open(args.file) as f:
+        for line in f:
+            print(line)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--a", type=str, required=True)
-    parser.add_argument("--b", type=str, required=True)
+    parser.add_argument("file", type=str, required=True)
     main(parser.parse_args())
